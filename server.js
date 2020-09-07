2fb6e955bfa9fa19f1a1cc0e9968ebc5f17e5b2f
rohan <rohanrajgautam@gmail.com>
2020-09-07 16:36:03 +0545

    :fire: initial express setup, and created routes for bootcamps

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Router files
+const bootcamps = require('./routes/bootcamps');
+
+//Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+//Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
