2dd8789e2002f0978b842f81de1bb569e8a4a0ff
rohan <rohanrajgautam@gmail.com>
2020-09-08 16:29:54 +0545

    connected to the database with mongoose, added colors npm for console coloring, created bootcamp model

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-//Router files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
+//Connect to the database
+connectDB();
+
+//Router files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Mount Routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+//handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
