547dcad496b0eedf66021639f2af0cc35939fb0f
rohan <rohanrajgautam@gmail.com>
2020-09-09 19:13:29 +0545

    added mongoose error handling

--- middleware/error.js
+++ middleware/error.js
@@ -14,9 +14,15 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 404);
   }
 
-  // Duplicate id error
+  // Mongoose Duplicate id
   if (err.code === 11000) {
     const message = 'Duplicate content found. Please try something new';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
     error = new ErrorResponse(message, 400);
   }
 
