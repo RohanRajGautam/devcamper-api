93670547c56c90d80d4e78923dae3f67081c1e3c
rohan <rohanrajgautam@gmail.com>
2020-09-10 11:37:09 +0545

    add geoJSON location and geocoder hook mapquest

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -97,16 +98,30 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: true,
-  },
 });
 
 // create bootcamp slug from the name
 BootcampSchema.pre('save', function () {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode and create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  //do not save address in db
+  this.address = undefined;
   next();
 });
 
