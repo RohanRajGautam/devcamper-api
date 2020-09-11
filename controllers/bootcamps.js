54e27a43ca2c71ba207dee7e65da9ef220250e25
rohan <rohanrajgautam@gmail.com>
2020-09-11 14:27:58 +0545

    added advanced filtering for get all bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,19 @@ const geocoder = require('../utils/geocoder');
 // @routes    GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamps.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamps.find(JSON.parse(queryStr));
+
+  const bootcamps = await query;
+
   res
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
