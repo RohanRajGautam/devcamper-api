b4b0f11affd8e91e464bb228237b96912006e0d3
rohan <rohanrajgautam@gmail.com>
2020-09-15 10:01:56 +0545

    added advancedResults for courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,24 +8,17 @@ const Bootcamp = require('../models/Bootcamps');
 // @routes    GET /api/v1/bootcamps/:bootcampId/courses
 // @access    Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
-
   if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Course.find().populate({
-      path: 'bootcamp',
-      select: 'name description',
+    const course = await Course.find({ bootcamp: req.params.bootcampId });
+
+    res.status(200).json({
+      success: true,
+      count: course.length,
+      data: course,
     });
+  } else {
+    res.status(200).json(res.advancedResults);
   }
-
-  const courses = await query;
-
-  res.status(200).json({
-    success: true,
-    count: courses.length,
-    data: courses,
-  });
 });
 
 // @desc      Get single course
