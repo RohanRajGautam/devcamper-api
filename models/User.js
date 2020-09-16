b6a19ac444902a20787038cc5bf80647e4a090d7
rohan <rohanrajgautam@gmail.com>
2020-09-16 09:45:18 +0545

    created user model and added jsonwebtoken

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'provider'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  CreatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
