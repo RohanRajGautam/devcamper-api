e6e97a2bd14c281994127b5c3b86d40ec3349af5
rohan <rohanrajgautam@gmail.com>
2020-09-16 10:28:24 +0545

    implemented user register

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,16 @@ const asyncHandler = require('../middleware/async');
 // @routes    GET /api/v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  //create a user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
   res.status(200).json({
     success: true,
   });
