fd8af19cb69f3587aaf1101a80d75671791c8e99
rohan <rohanrajgautam@gmail.com>
2020-09-18 08:16:29 +0545

    created user field in the seeder

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 //load the models
 const Bootcamp = require('./models/Bootcamps');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 //connect to the db
 mongoose.connect(process.env.MONGO_URI, {
@@ -26,11 +27,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 //import into db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log('Data imported'.green.inverse);
     process.exit();
   } catch (err) {
@@ -43,6 +49,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log('Data deleted'.red.inverse);
     process.exit();
   } catch (err) {
