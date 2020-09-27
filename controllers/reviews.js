cc3b0791fb8048cd4dfe7ec8255a8b8540ca68ba
rohan <rohanrajgautam@gmail.com>
2020-09-27 16:36:07 +0545

    delete review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -95,3 +95,28 @@ exports.updateReview = asyncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+// @desc      Delete review
+// @route     DELETE /api/v1/reviews/:id
+// @access    Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  await review.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,7 @@ const {
   getReview,
   addReview,
   updateReview,
+  deleteReview,
 } = require('../controllers/reviews');
 const Review = require('../models/Review');
 
@@ -26,6 +27,7 @@ router
 router
   .route('/:id')
   .get(getReview)
-  .put(protect, authorize('user', 'admin'), updateReview);
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
